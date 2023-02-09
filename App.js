f2693e4724ee27ee494a7dd4982aa03587b8cb8b
Aziz7477 <95928540+Aziz7477@users.noreply.github.com>
2023-02-09 21:41:01 +0530

    first

--- App.js
+++ App.js
@@ -1,1 +1,45 @@
 
+let button = document.getElementById("btn2");
+let card = document.getElementById("card");
+let saveItem = document.querySelector("#save-item");
+let sound = document.getElementById("sound");
+
+let searches = [];
+button.addEventListener("click",()=>{
+  let inputWord = document.getElementById("input-Word").value;
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`)
+  .then((response) => response.json())
+  .then((data) => {
+    card.innerHTML = `<div>Word : <span>${inputWord}</span></div>
+    <div id="details">${data[0].meanings[0].partOfSpeech}</div>
+    <button onclick="playSound()">
+        <i id="play" class="fa-solid fa-volume-high"></i>
+    </button>
+    <div>Defination : <span>${data[0].meanings[0].definitions[0].definition}</span></div>
+    `;
+    sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
+
+    searches.push({Word:inputWord,meaning:data[0].meanings[0].definitions[0].definition})
+    localStorage.setItem("search",JSON.stringify(searches))
+  })
+  .catch(()=>{
+    card.innerHTML = "<h4>Couldn't Find The Word</h4>"
+  })
+})
+function playSound(){
+  sound.play();
+}
+new1 = document.getElementsByClassName("new")
+localStorage.setItem("name2","aziz")
+let his = localStorage.getItem("name2")
+function renderSearch(){
+  new1.innerHTML = "";
+  his.forEach((searchTerm)=>{
+    const li = document.createElement("li")
+    li.innerHTML = searchTerm;
+    new1.appendChild(li);
+    console.log("hi")
+  })
+}
+renderSearch();
+
