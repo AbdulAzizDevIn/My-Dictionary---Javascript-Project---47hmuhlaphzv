bb5a32177c76ae92a2625329bd526e2fb51d98cd
Aziz7477 <95928540+Aziz7477@users.noreply.github.com>
2023-02-17 22:58:06 +0530

    add loading screen

--- App.js
+++ App.js
@@ -1,78 +1,84 @@
-
 let button = document.getElementById("btn2");
 let card = document.getElementById("card");
 let saveItem = document.querySelector("#save-item");
 let sound = document.getElementById("sound");
 
-button.addEventListener("click",()=>{
+button.addEventListener("click", () => {
   let inputWord = document.getElementById("input-Word").value;
-  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`)
-  .then((response) => response.json())
-  .then((data) => {
-    card.innerHTML = `<div>Word : <span>${inputWord}</span></div>
+  if (inputWord == "") {
+    card.innerHTML = "<div>Please enter any word</div>";
+  } else {
+    card.innerHTML = "<img src='img/loding.gif'>";
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`)
+      .then((response) => response.json())
+      .then((data) => {
+        card.innerHTML = `<div>Word : <span>${inputWord}</span></div>
     <div id="details">${data[0].meanings[0].partOfSpeech}</div>
     <button onclick="playSound()">
         <i id="play" class="fa-solid fa-volume-high"></i>
     </button>
     <div>Defination : <span>${data[0].meanings[0].definitions[0].definition}</span></div>
     `;
-    sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
+        sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
 
-    localStorage.setItem(`${inputWord}`,`${data[0].meanings[0].definitions[0].definition}`)
-  })
-  .catch(()=>{
-    card.innerHTML = "<h4>Couldn't Find The Word</h4>"
-  })
-})
+        localStorage.setItem(
+          `${inputWord}`,
+          `${data[0].meanings[0].definitions[0].definition}`
+        );
+      })
+      .catch(() => {
+        card.innerHTML = "<h4>Couldn't Find The Word</h4>";
+      });
+  }
+});
 
-function playSound(){
+function playSound() {
   sound.play();
 }
 
 const historybtn = document.getElementById("historybtn");
 const historyPage = document.querySelector(".historyPage");
-const main = document.querySelector(".main")
+const main = document.querySelector(".main");
 
-historybtn.addEventListener("click",()=>{
-  if(historybtn.innerText == "HISTORY"){
+historybtn.addEventListener("click", () => {
+  if (historybtn.innerText == "HISTORY") {
     document.querySelector(".search").style.display = "none";
     card.style.display = "none";
     historyPage.style.display = "flex";
-    if(localStorage.length == 0){
-      historyPage.innerHTML = "<div class = 'empty'><img src='https://cdn-icons-png.flaticon.com/512/1380/1380641.png' alt='image'><span>No Search Found</span></div>";
-
+    if (localStorage.length == 0) {
+      historyPage.innerHTML =
+        "<div class = 'empty'><img src='https://cdn-icons-png.flaticon.com/512/1380/1380641.png' alt='image'><span>No Search Found</span></div>";
     }
     historybtn.innerHTML = "SEARCH";
-    document.querySelector(".name").innerHTML = "<span>My DICTIONARY App</span><div> History</div>";
-    for(let i=0; i<localStorage.length; i++){
-      if(localStorage.key(i) == "count"){
+    document.querySelector(".name").innerHTML =
+      "<span>My DICTIONARY App</span><div> History</div>";
+    for (let i = 0; i < localStorage.length; i++) {
+      if (localStorage.key(i) == "count") {
         continue;
       }
       let div = document.createElement("div");
-      div.setAttribute("class","newDiv");
-      div.innerHTML = `<span>Word: <span class="getdata">${localStorage.key(i)}</span></span>
+      div.setAttribute("class", "newDiv");
+      div.innerHTML = `<span>Word: <span class="getdata">${localStorage.key(
+        i
+      )}</span></span>
       <br>
       <p>${localStorage.getItem(localStorage.key(i))}</p>
       <i onclick="deleteDiv(this)" id="dlt" class="fa-solid fa-trash"></i>`;
       historyPage.appendChild(div);
     }
-  }
-  else if(historybtn.innerText == "SEARCH"){
+  } else if (historybtn.innerText == "SEARCH") {
     document.querySelector(".name").innerText = "My DICTIONARY App";
     historyPage.innerHTML = "";
     historyPage.style.display = "none";
     document.querySelector(".search").style.display = "flex";
     card.style.display = "grid";
     historybtn.innerText = "HISTORY";
-
   }
-})
+});
 
-function deleteDiv(currentEl){
+function deleteDiv(currentEl) {
   let key = currentEl.parentElement.querySelector(".getdata").innerText;
   currentEl.parentElement.remove();
 
   localStorage.removeItem(key);
 }
-
-
